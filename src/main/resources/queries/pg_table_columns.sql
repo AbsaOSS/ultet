466ab202b5668703251ac0207ddcbf6fd0e8af52
David Benedeki <benedeki@volny.cz>
2024-02-03 23:31:27 +0100

    * addressed PR comments

--- src/main/resources/queries/pg_table_columns.sql
+++ src/main/resources/queries/pg_table_columns.sql
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A query that returns all columns of a table chosen by its name and schema.
+ */
 SELECT
     N.nspname AS schema_name,
     T.relname AS table_name,
@@ -23,4 +41,4 @@ WHERE
     AND N.nspname = ?
     AND T.relname = ?
 ORDER BY
-    C.ordinal_position
+    C.ordinal_position;
--- src/main/resources/queries/pg_table_properties.sql
+++ src/main/resources/queries/pg_table_properties.sql
@@ -1,4 +1,22 @@
-SELECT N.nspname AS schema_name,
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A query that returns properties of a table chosen by its name and schema.
+ */
+ SELECT N.nspname AS schema_name,
        T.relname AS table_name,
        pg_get_userbyid(T.relowner) AS table_owner,
        T.relhasindex AS has_indexes,
@@ -9,4 +27,4 @@ FROM pg_class T
 WHERE
     T.relkind = ANY (ARRAY['r'::"char", 'p'::"char"])
     AND N.nspname = ?
-    AND T.relname = ?
+    AND T.relname = ?;
